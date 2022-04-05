7dec686a52114f9b9a87cc95efb993cf0fcc4e75
waltvin <waltvin.l@gmail.com>
2022-04-04 18:07:33 -0700

    fix crosshair getting stuck at bottom

--- motion.py
+++ motion.py
@@ -9,31 +9,27 @@ test_frame_file = "./scene_images/frame_55.038.png"
 
 prev_frame = None
 
+
 def CallMotion(current_frame, test_mode=False):
     global prev_frame
-
-    if prev_frame is None:
-        prev_frame = current_frame
-        return None
 
     # 1. Load image
     img_bgr = current_frame
     # img_rgb = cv2.cvtColor(src=img_bgr, code=cv2.COLOR_BGR2RGB)
-    previous_img_bgr = prev_frame.copy()
-    # previous_img_rgb = cv2.cvtColor(src=previous_img_bgr, code=cv2.COLOR_BGR2RGB)
-    prev_frame = current_frame.copy()
 
     # 2. Prepare image; grayscale and blur
     prepared_frame = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
     prepared_frame = cv2.GaussianBlur(
         src=prepared_frame, ksize=(5, 5), sigmaX=0)
-    prepared_frame_previous = cv2.cvtColor(
-        previous_img_bgr, cv2.COLOR_BGR2GRAY)
-    prepared_frame_previous = cv2.GaussianBlur(
-        src=prepared_frame_previous, ksize=(5, 5), sigmaX=0)
+
+    if prev_frame is None:
+        prev_frame = prepared_frame
+        return None
 
     # calculate difference and update previous frame
-    diff_frame = cv2.absdiff(src1=prepared_frame_previous, src2=prepared_frame)
+    diff_frame = cv2.absdiff(src1=prev_frame, src2=prepared_frame)
+
+    prev_frame = prepared_frame
 
     # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
     kernel = np.ones((5, 5))
@@ -45,15 +41,19 @@ def CallMotion(current_frame, test_mode=False):
 
     contours, _ = cv2.findContours(
         image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    if test_mode:
+    if True:
         cv2.drawContours(image=img_bgr, contours=contours, contourIdx=-1,
-                     color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+                         color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
 
     for contour in contours:
         # if cv2.contourArea(contour) < 50:
         #     # too small: skip!
         #     continue
         (x, y, w, h) = cv2.boundingRect(contour)
+
+        # case where crosshair gets stuck at the bottom
+        if y > current_frame.shape[0] - 42:
+            continue
 
         # if contour is around a box of size 40-42 (crosshair), skip
         # usually w and h around 40 - 42
@@ -64,12 +64,11 @@ def CallMotion(current_frame, test_mode=False):
         # if the contour is vertical means change somewhat the y direction, then it is a duck dying, skip
         # however if the alive duck is flying somewhat vertically they unfortunately get filtered out too.
         if w < h:
-            print(w, h)
             continue
 
         if not test_mode:
             return ((x + (x + w)) / 2, (y + (y + w)) / 2)
-        
+
         cv2.rectangle(img=img_bgr, pt1=(x, y), pt2=(
             x + w, y + h), color=(0, 255, 0), thickness=2)
 
