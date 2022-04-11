0bc389c056e95cd19d45ec910dce68c416d1a344
waltvin <waltvin.l@gmail.com>
2022-04-10 18:08:11 -0700

    Refactor SIFT and motion/background-subtraction code, added threshold for rain scene, fixed Lowe's ratio for SIFT, renamed SIFT duck folder name

--- motion.py
+++ motion.py
@@ -1,3 +1,4 @@
+# Motion Detection or Background Subtraction steps referred from : 
 # https://towardsdatascience.com/image-analysis-for-beginners-creating-a-motion-detector-with-opencv-4ca6faba4b42
 
 import cv2
@@ -13,11 +14,16 @@ prev_frame = None
 def CallMotion(current_frame, test_mode=False):
     global prev_frame
 
-    # 1. Load image
+    # For Testing    
+    # prev_frame = cv2.imread(image_2)
+    # prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    # prev_frame = cv2.GaussianBlur(
+    #     src=prev_frame, ksize=(5, 5), sigmaX=0)
+    
+    # Load image
     img_bgr = current_frame
-    # img_rgb = cv2.cvtColor(src=img_bgr, code=cv2.COLOR_BGR2RGB)
 
-    # 2. Prepare image; grayscale and blur
+    # Prepare image; grayscale and blur
     prepared_frame = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
     prepared_frame = cv2.GaussianBlur(
         src=prepared_frame, ksize=(5, 5), sigmaX=0)
@@ -26,43 +32,54 @@ def CallMotion(current_frame, test_mode=False):
         prev_frame = prepared_frame
         return None
 
-    # calculate difference and update previous frame
+    # Calculate difference
     diff_frame = cv2.absdiff(src1=prev_frame, src2=prepared_frame)
-
+    
+    if test_mode:
+        cv2.imshow('Motion detector', diff_frame)
+        cv2.waitKey()
+    
+    # Update previous frame
     prev_frame = prepared_frame
 
-    # 4. Dilute the image a bit to make differences more seeable; more suitable for contour detection
+    # Dilute the image a bit to make differences more seeable; more suitable for contour detection
     kernel = np.ones((5, 5))
     diff_frame = cv2.dilate(diff_frame, kernel, 1)
-
-    # 5. Only take different areas that are different enough (>20 / 255)
-    thresh_frame = cv2.threshold(
-        src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+    
+    if test_mode:
+        cv2.imshow('Frame Difference', diff_frame)
+        cv2.waitKey()
 
     contours, _ = cv2.findContours(
-        image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    if True:
+        image=diff_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+    if test_mode:
         cv2.drawContours(image=img_bgr, contours=contours, contourIdx=-1,
                          color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+        cv2.imshow('Contours Drawn', img_bgr)
+        cv2.waitKey()
 
     for contour in contours:
-        # if cv2.contourArea(contour) < 50:
-        #     # too small: skip!
-        #     continue
+        # During the demo, there were levels with rain,
+        # Adding this countour area check to make sure they are above a certain size basically removed all rain false positives.
+        if cv2.contourArea(contour) < 800:
+            # too small: skip!
+            continue
+        
+        print(cv2.contourArea(contour))
         (x, y, w, h) = cv2.boundingRect(contour)
 
-        # case where crosshair gets stuck at the bottom
+        # This is a case where crosshair gets stuck at the bottom
+        # Happens because if crosshair is at the bottom, when it moves, the bounding box will be flat
         if y > current_frame.shape[0] - 42:
             continue
 
-        # if contour is around a box of size 40-42 (crosshair), skip
-        # usually w and h around 40 - 42
+        # If contour is around a box of size 40-42 (crosshair), skip
+        # Usually w and h around 40 - 42
         if 38 <= w <= 42 and 38 <= h <= 42:
             continue
 
-        # hacky method, probably needs improvement
-        # if the contour is vertical means change somewhat the y direction, then it is a duck dying, skip
-        # however if the alive duck is flying somewhat vertically they unfortunately get filtered out too.
+        # Falling ducks are being detected and being shot at. 
+        # Thus, bounding boxes that are shaped vertically with their heights being more than their width, are filtered out
         if w < h:
             continue
 
@@ -73,7 +90,7 @@ def CallMotion(current_frame, test_mode=False):
             x + w, y + h), color=(0, 255, 0), thickness=2)
 
     if test_mode:
-        cv2.imshow('Motion detector', img_bgr)
+        cv2.imshow('', img_bgr)
         cv2.waitKey()
 
 
